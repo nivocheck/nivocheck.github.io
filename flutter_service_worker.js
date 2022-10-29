24d4e765a6cc3d366d1c09a9d77d3eb4c43245c9
Andrey Maier <admin@alpclub.de>
2022-10-29 23:27:37 +0200

    Updated to 1.1.6 preview

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "e48144e7db214ba4ac3c1b7da53eb560",
-"index.html": "6e9a26bca726c5d7c57606db52cfe882",
-"/": "6e9a26bca726c5d7c57606db52cfe882",
-"main.dart.js": "137bf84735db14a1fdc8d33220ec9218",
+  "version.json": "8d187ece7513cb9b745109df4a7cbef5",
+"index.html": "d444b64dafc583cae6c1c576f406b774",
+"/": "d444b64dafc583cae6c1c576f406b774",
+"main.dart.js": "6a70a057bed681a15ac1dc0bc7671109",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "14268a0a8033940154ad3f8ec6586f28",
 "icons/Icon-192.png": "6e600f5d4278c97f793cafad4c674f06",
